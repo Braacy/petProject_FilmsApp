934180cc29098eab6a6088c0a7a666d1d0404860
Vladislav Bliznyuk <braacy@yandex.ru>
2024-09-17 18:59:38 +0200

    updateAnimationClose

--- FilmsApp/RoundingTransition.swift
+++ FilmsApp/RoundingTransition.swift
@@ -9,24 +9,24 @@ import Foundation
 import UIKit
 
 class RoundingTransition: NSObject {
-    
     var round = UIView()
     
     var start = CGPoint.zero {
-        didSet{
+        didSet {
             round.center = start
         }
     }
     
-    var time = 0.5
-    
     var roundColor = UIColor.red
     
-    enum RoundingTransitionProfile {
+    var time = 0.3
+    
+    enum RoundingTransitionProfile: Int {
         case show, cancel, pop
     }
     
     var transitionProfile: RoundingTransitionProfile = .show
+    
 }
 
 extension RoundingTransition: UIViewControllerAnimatedTransitioning {
@@ -36,6 +36,7 @@ extension RoundingTransition: UIViewControllerAnimatedTransitioning {
     }
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
         let container = transitionContext.containerView
         
         if transitionProfile == .show {
@@ -60,63 +61,61 @@ extension RoundingTransition: UIViewControllerAnimatedTransitioning {
                 container.addSubview(showedView)
                 
                 UIView.animate(withDuration: time, animations: {
-                    
+                    // часть 1
                     self.round.transform = CGAffineTransform.identity
                     showedView.transform = CGAffineTransform.identity
                     showedView.alpha = 1
                     showedView.center = viewCenter
                     
+                    // часть 2
                 }, completion: { (success: Bool) in
                     transitionContext.completeTransition(success)
                 })
+            }
+        }else{
+            let transitionModeKey = (transitionProfile == .pop) ? UITransitionContextViewKey.to : UITransitionContextViewKey.from
+            
+            if let returnableView = transitionContext.view(forKey: transitionModeKey) {
                 
-            } else {
+                let viewCenter = returnableView.center
+                let viewSize = returnableView.frame.size
                 
-                let transitionModeKey = (transitionProfile == .pop) ? UITransitionContextViewKey.to : UITransitionContextViewKey.from
-                if let returnableView = transitionContext.view(forKey: transitionModeKey) {
-                    let viewCenter = returnableView.center
-                    let viewSize = returnableView.frame.size
+                round.frame = roundFrame(withViewCenter: viewCenter, size: viewSize, startPoint: start)
+                
+                round.layer.cornerRadius = round.frame.size.height / 2
+                round.center = start
+                
+                UIView.animate(withDuration: time, animations: {
                     
-                    round.frame = roundFrame(withViewCenter: viewCenter, size: viewSize, startPoint: start)
+                    self.round.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+                    returnableView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+                    returnableView.center = self.start
+                    returnableView.alpha = 0
                     
-                    round.layer.cornerRadius = round.frame.size.height / 2
-                    round.center = start
+                    if self.transitionProfile == .pop {
+                        container.insertSubview(returnableView, belowSubview: returnableView)
+                        container.insertSubview(self.round, belowSubview: returnableView)
+                    }
                     
-                    UIView.animate(withDuration: time, animations: {
-                        self.round.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
-                        returnableView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
-                        returnableView.center = self.start
-                        returnableView.alpha = 0
-                        
-                        if self.transitionProfile == .pop {
-                            container.insertSubview(returnableView, belowSubview: returnableView)
-                            container.insertSubview(self.round, belowSubview: returnableView)
-                        }
-                        
-                    }, completion: { (success: Bool) in
-                        returnableView.center = viewCenter
-                        returnableView.removeFromSuperview()
-                        
-                        self.round.removeFromSuperview()
-                        
-                        transitionContext.completeTransition(success)
-                    })
+                }, completion: { (success: Bool) in
+                    returnableView.center = viewCenter
+                    returnableView.removeFromSuperview()
                     
+                    self.round.removeFromSuperview()
                     
-                }
+                    transitionContext.completeTransition(success)
+                })
             }
         }
     }
     
-        func roundFrame(withViewCenter viewCenter: CGPoint, size viewSize: CGSize, startPoint: CGPoint) -> CGRect {
-            let xLength = fmax(startPoint.x, viewSize.width - startPoint.x)
-            let yLength = fmax(startPoint.y, viewSize.height - startPoint.y)
-            
-            let offsetVector = sqrt(xLength * xLength + yLength * yLength) * 2
-            let size = CGSize(width: offsetVector, height: offsetVector)
-            
-            return CGRect(origin: CGPoint.zero, size: size)
-            
- }
-    
+    func roundFrame(withViewCenter viewCenter: CGPoint, size viewSize: CGSize, startPoint: CGPoint) -> CGRect {
+        let xLength = fmax(startPoint.x, viewSize.width - startPoint.x)
+        let yLength = fmax(startPoint.y, viewSize.height - startPoint.y)
+        
+        let offsetVector = sqrt(xLength * xLength + yLength * yLength) * 2
+        let size = CGSize(width: offsetVector, height: offsetVector)
+        
+        return CGRect(origin: CGPoint.zero, size: size)
+    }
 }
